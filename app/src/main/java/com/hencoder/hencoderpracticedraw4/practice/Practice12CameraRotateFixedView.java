8049a33c92357ef580f74599fcbf2c40cc588e6c
renbo <renbo.ma@hypers.com>
2017-08-10 18:42:30 +0800

    update

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -5,7 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Camera;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
@@ -23,13 +23,29 @@ public class Practice12CameraRotateFixedView extends View {
     Camera mCamera = new Camera();
     Path mPath = new Path();
 
+    private float mGap = 0;
+    float p = 50;
+
     public Practice12CameraRotateFixedView(Context context) {
         super(context);
     }
 
-    public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs) {
+    public Practice12CameraRotateFixedView(final Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mGap >= 500) {
+                    p = -50;
+                } else if (mGap < 0) {
+                    p = 50;
+                }
+                mGap += p;
+                invalidate();
+            }
+        });
     }
 
     public Practice12CameraRotateFixedView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -38,9 +54,11 @@ public class Practice12CameraRotateFixedView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
-//        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
     }
 
+
+    Matrix mMatrix = new Matrix();
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -70,23 +88,79 @@ public class Practice12CameraRotateFixedView extends View {
 //        canvas.drawPath(mPath, paint);
 //        canvas.restore();
 
-
-        paint.setColor(Color.BLACK);
+//        float wX = 200 + bitmap.getWidth() / 2;
+//        float wY = 200 + bitmap.getHeight() / 2;
+//        paint.setColor(Color.BLACK);
+//        canvas.save();
+//        mCamera.save();
+//        mCamera.getMatrix(mMatrix);
+//        Log.d("Practice12CameraRotateF", "mGap:" + mGap);
+//        mMatrix.postTranslate(0, mGap);
+//        mCamera.translate(0, 0, mGap);
+//        mCamera.rotateX(45);
+//        mCamera.applyToCanvas(canvas);
+//
+//
+////        paint.setColor(Color.BLUE);
+////        canvas.drawPath(mPath, paint);
+//
+//
+//        mCamera.restore();
+//        canvas.drawBitmap(bitmap, 0, 0, paint);
+//        canvas.restore();
+//
+//
+//        Matrix nM = new Matrix();
+//        canvas.save();
+//        paint.setStrokeWidth(20);
+//        canvas.drawPoint(600, 200, paint);
+//
+//
+//        nM.preTranslate(100, 100);
+//        canvas.setMatrix(nM);
+//        paint.setColor(Color.RED);
+//        canvas.drawPoint(600, 200, paint);
+//
+//        nM.postScale(0.5f, 0.5f, 600, 200);
+//        canvas.setMatrix(nM);
+//        paint.setColor(Color.YELLOW);
+//        canvas.drawPoint(600, 200, paint);
+//
+//
+//        nM.postTranslate(100, 100);
+//        paint.setColor(Color.BLUE);
+//        canvas.setMatrix(nM);
+//        canvas.drawPoint(600, 200, paint);
+//        canvas.restore();
+//        canvas.drawBitmap(bitmap, 216 - bitmap.getWidth() / 2, 216 - bitmap.getHeight() / 2, paint);
         canvas.save();
+        mMatrix.reset();
+
+//        paint.setStrokeWidth(1);
+//        canvas.drawLine(144, 144, 144, getHeight(), paint);
+//        canvas.drawLine(144, 144, getWidth(), 144, paint);
+//        canvas.drawLine(0, 0, 500, 500, paint);
+        mCamera.getMatrix(mMatrix);
         mCamera.save();
+//        mCamera.translate(216, -216, 0);
+//        float xx = (float) (bitmap.getWidth() / 2) / 72;
+//        mCamera.setLocation(xx, 0, -8);
+
+//        canvas.translate(bitmap.getWidth() / 2, bitmap.getHeight() / 2);
         mCamera.rotateX(45);
-        canvas.translate(200 + bitmap.getWidth() / 2, 200 + bitmap.getHeight() / 2);
-        canvas.drawPath(mPath, paint);
+//        mCamera.translate(-100, 0, 0);
         mCamera.applyToCanvas(canvas);
-        canvas.translate(-200 - bitmap.getWidth() / 2, -200 - bitmap.getHeight() / 2);
-        paint.setColor(Color.BLUE);
-        canvas.drawPath(mPath, paint);
+//        canvas.translate(-bitmap.getWidth() / 2,- bitmap.getHeight() / 2);
         mCamera.restore();
 
-        canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+//        paint.setStyle(Paint.Style.FILL);
+//        canvas.drawCircle(0, 0, 50, paint);
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+//        canvas.translate(500, 500);
         canvas.restore();
 
 
-        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
     }
+
+
 }
